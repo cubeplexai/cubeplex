4308acbb7e82f3db4452f5aef056cb88bd277d7c
xfgong <gxf.alpha@gmail.com>
2026-03-17 10:27:45 +0800

    feat(models): add Message model

--- backend/cubebox/models/__init__.py
+++ backend/cubebox/models/__init__.py
@@ -1,5 +1,6 @@
 """Data models."""
 
 from cubebox.models.conversation import Conversation
+from cubebox.models.message import Message
 
-__all__ = ["Conversation"]
+__all__ = ["Conversation", "Message"]
