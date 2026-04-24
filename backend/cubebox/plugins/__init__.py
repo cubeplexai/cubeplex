b111bdc65a49c0ad8e368916fc860763c8ee568e
xfgong <gxf.alpha@gmail.com>
2026-04-24 10:32:45 +0800

    chore(plugins): create package skeleton for M0 plugin architecture

---
+++ backend/cubebox/plugins/__init__.py
@@ -0,0 +1,5 @@
+"""CE/EE plugin protocols + entry_points-based discovery.
+
+This package defines the contracts that external plugins (e.g. cubebox-ee)
+implement to extend cubebox CE. See docs/superpowers/specs/2026-04-22-ce-ee-plugin-architecture-design.md.
+"""
