1caa8a1d92465a3df647bb9d3f97541631c2bd91
xfgong <gxf.alpha@gmail.com>
2026-06-19 12:48:20 +0800

    feat(im): teams platform connector + registration

--- backend/cubebox/im/teams/__init__.py
+++ backend/cubebox/im/teams/__init__.py
@@ -1,1 +1,6 @@
 """Microsoft Teams IM connector."""
+
+from cubebox.im.registry import register_platform
+from cubebox.im.teams._platform import TeamsPlatform
+
+register_platform("teams", TeamsPlatform())
